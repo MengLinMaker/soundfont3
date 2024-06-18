bad6bca931bba7d1bed0bd7d7b7e18a22a7a76cd
Meng Lin <menglinmaker@gmail.com>
2024-06-19 00:31:38 +1000

    DX: Use typescript for latest jest config

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+const config: Config = {
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+  testMatch: ['**/tests/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js'],
+  moduleNameMapper: {
+    '~/(.*)': '<rootDir>/src/$1'
+  },
+  setupFiles: ['<rootDir>/jest.setup.ts']
+};
+module.exports = config;
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,3 @@
 // Required for Jest to work on older Node.js versions
+// @ts-ignore
 global.TextDecoder = require('util').TextDecoder;
