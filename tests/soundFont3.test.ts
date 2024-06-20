343b381d36989fcdb222d4285727df452cb198aa
Meng Lin <menglinmaker@gmail.com>
2024-06-20 16:55:38 +1000

    DX: Add SF3 RIFF file validation test

---
+++ tests/soundFont3.test.ts
@@ -0,0 +1,58 @@
+import { join } from 'path'
+import { SoundFont3 } from '../src'
+import { readFileSync } from 'fs'
+import { Soundfont2Sampler } from 'smplr'
+import { bufferToDataUrl } from './bufferToDataUrl'
+import { RIFFFile } from 'riff-file'
+
+const soundFontUrl = join(__dirname, 'fonts/piano.sf3')
+const buffer = readFileSync(soundFontUrl)
+
+describe('SoundFont3', () => {
+  it('should parse as a RIFF file', () => {
+    const riff = new RIFFFile()
+    riff.setSignature(buffer)
+    const expectedRiffSignaturePath = join(__dirname, 'fonts/piano.sf3.riff.signature.json')
+    const expectedRiffSignature = readFileSync(expectedRiffSignaturePath).toString()
+    const riffSignature = JSON.stringify(riff.signature, null, 2)
+    expect(riffSignature).toStrictEqual(expectedRiffSignature)
+  })
+
+  // it('should parse metadata', () => {
+  //   const soundFont = new SoundFont3(buffer)
+  //   const metaData = soundFont.metaData
+  //   expect(metaData).toStrictEqual({
+  //     version: '2.1',
+  //     soundEngine: 'EMU8000',
+  //     name: 'Yamaha-Grand-Lite',
+  //     rom: undefined,
+  //     romVersion: undefined,
+  //     creationDate: undefined,
+  //     author: undefined,
+  //     product: undefined,
+  //     copyright: 'Creative Commons',
+  //     comments: undefined,
+  //     createdBy: 'Polyphone'
+  //   })
+  // })
+
+  // it('should load into "smplr"', async () => {
+  //   const context = new AudioContext()
+  //   const sampler = new Soundfont2Sampler(context, {
+  //     url: bufferToDataUrl(buffer),
+  //     createSoundfont: (data) => new SoundFont3(data)
+  //   })
+  //   sampler.load.then(() => {
+  //     sampler.loadInstrument(sampler.instrumentNames[0])
+  //     expect(sampler.instrumentNames).toStrictEqual([
+  //       'Sal-L1',
+  //       'Sal-L2',
+  //       'Sal-L3',
+  //       'Sal-L4',
+  //       'Sal-L5',
+  //       'Sal-L6',
+  //       'Sal-L7'
+  //     ])
+  //   })
+  // })
+})
