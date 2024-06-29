a8e7bb95b7f6d0f15cebda15165c736b3c16ce66
MengLinMaker <menglinmaker@gmail.com>
2024-06-29 22:33:04 +1000

    DX: Extract audio data for testing and modification

--- src/soundFont3.ts
+++ src/soundFont3.ts
@@ -13,6 +13,8 @@ import { SF2Chunk } from './chunk'
 import { parseBuffer, ParseError } from './riff'
 import { getItemsInZone } from './chunks'
 import { memoize } from './utils'
+import { pcm16BufferToWav } from './wav'
+import { writeFileSync } from 'fs'
 
 export class SoundFont3 {
   /**
@@ -142,6 +144,26 @@ export class SoundFont3 {
 
       return null
     })(memoizedKeyNumber, memoizedBankNumber, memoizedPresetNumber)
+  }
+
+  /**
+   * Extract samples from SoundFont to folder
+   * @param folderPath {string} - folder to dump samples.
+   * @returns SoundFont3 - enable chained commands
+   */
+  public async sampleToAudioFile(folderPath: string) {
+    const soundFontVersion = Number(this.metaData.version)
+    if (soundFontVersion >= 2 && soundFontVersion < 3) {
+      this.samples.forEach((sample) => {
+        const wavFile = pcm16BufferToWav(sample.header.sampleRate, sample.data)
+        writeFileSync(`${folderPath}/${sample.header.name}.wav`, wavFile)
+      })
+    } else if (soundFontVersion >= 3 && soundFontVersion < 4) {
+      this.samples.forEach((sample) => {
+        writeFileSync(`${folderPath}/${sample.header.name}.ogg`, sample.data)
+      })
+    }
+    return this
   }
 
   /**
---
+++ src/wav.ts
@@ -0,0 +1,55 @@
+/**
+ * Inserts a string into buffer at offset.
+ * @param view {DataView} -DataView of a buffer.
+ * @param offset {number} - offset in DataView.
+ * @param string {string} - string to insert into DataView.
+ */
+const dataViewWriteString = (view: DataView, offset: number, string: string) => {
+  for (let i = 0; i < string.length; i++) {
+    view.setUint8(offset + i, string.charCodeAt(i))
+  }
+}
+
+/**
+ * Converts a pcm int16 audio data to WAV file as a buffer.
+ * @param sampleRate {number} - sample rate in Hertz - eg: 44100 Hz.
+ * @param pcm16Buffer {Int16Array} - pcm int16 audio data.
+ * @returns wavFileBuffer - WAV RIFF file as binary.
+ */
+export function pcm16BufferToWav(sampleRate: number, pcm16Buffer: Int16Array) {
+  const channelCount = 1
+
+  const headerBuffer = new ArrayBuffer(44)
+  const headerView = new DataView(headerBuffer)
+
+  /* RIFF identifier */
+  dataViewWriteString(headerView, 0, 'RIFF')
+  /* RIFF chunk length */
+  headerView.setUint32(4, 44 + pcm16Buffer.byteLength, true)
+  /* RIFF type */
+  dataViewWriteString(headerView, 8, 'WAVE')
+  /* format chunk identifier */
+  dataViewWriteString(headerView, 12, 'fmt ')
+  /* format chunk length */
+  headerView.setUint32(16, 16, true)
+  /* sample format (raw) */
+  headerView.setUint16(20, 1, true)
+  /* channel count */
+  headerView.setUint16(22, channelCount, true)
+  /* sample rate */
+  headerView.setUint32(24, sampleRate, true)
+  /* byte rate (sample rate * block align) */
+  headerView.setUint32(28, sampleRate * channelCount * 2, true)
+  /* block align (channel count * bytes per sample) */
+  headerView.setUint16(32, channelCount * 2, true)
+  /* bits per sample */
+  headerView.setUint16(34, 16, true)
+  /* data chunk identifier */
+  dataViewWriteString(headerView, 36, 'data')
+  /* data chunk length */
+  headerView.setUint32(40, 8 + pcm16Buffer.byteLength, true)
+
+  // Append pcm16 data
+  const wavFileBuffer = Buffer.concat([Buffer.from(headerBuffer), Buffer.from(pcm16Buffer)])
+  return wavFileBuffer
+}
