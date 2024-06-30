b071c406d6b89fe7a32e37d1eafa9bb38bd56d0a
MengLinMaker <menglinmaker@gmail.com>
2024-07-01 00:34:42 +1000

    DX: Reorganise chunks files based on functionality

--- src/chunks/index.ts
+++ src/chunks/index.ts
@@ -1,6 +1,4 @@
 export * from './instruments'
 export * from './presets'
 export * from './samples'
-export * from './generators'
-export * from './modulators'
-export * from './zones'
+export * from './parsers'
---
+++ src/chunks/parsers/index.ts
@@ -0,0 +1,3 @@
+export * from './generators'
+export * from './modulators'
+export * from './zones'
