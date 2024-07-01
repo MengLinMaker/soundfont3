95f86a067adaf279ddd6b048ed81ee58cbe3a2a0
MengLinMaker <menglinmaker@gmail.com>
2024-07-01 22:53:10 +1000

    DX: Test throw error on invalid RIFF format

--- src/riff/test/chunkIterator.test.ts
+++ src/riff/test/chunkIterator.test.ts
@@ -1,15 +1,25 @@
+import { ParseError } from '../parseError'
 import { parseBuffer } from '../parser'
 import buffer from './mocks/buffer'
 
-const chunk = parseBuffer(buffer)
-
 describe('ChunkIterator', () => {
   it('should increase the position on reading', () => {
+    const chunk = parseBuffer(buffer)
     const iterator = chunk.iterator()
     expect(iterator.getString(4)).toBe('sfbk')
     expect(iterator.currentPosition).toBe(4)
     expect(iterator.getString(4)).toBe('LIST')
     expect(iterator.getUInt32()).toBe(8)
     expect(iterator.currentPosition).toBe(12)
+  })
+
+  it('should not parse invalid chunk ID', () => {
+    const invalidBuffer = Buffer.from(buffer)
+    invalidBuffer[0] = 0x0
+    try {
+      parseBuffer(invalidBuffer)
+    } catch (e) {
+      expect(e).toStrictEqual(new ParseError('Invalid file format'))
+    }
   })
 })
