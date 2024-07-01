e8230b0e727f8b005dcbfb291019013b7dbcea38
MengLinMaker <menglinmaker@gmail.com>
2024-07-02 02:16:34 +1000

    DX: Add helpers for writing RIFF file

--- src/write/utils.ts
+++ src/write/utils.ts
@@ -1,3 +1,5 @@
+import { SF_SUB_CHUNKS_ID, SF_TOP_CHUNKS_FORMAT, SF_TOP_CHUNKS_ID } from '../constants'
+
 /**
  * Inserts a string into buffer at offset.
  * @param view {DataView} -DataView of a buffer.
@@ -12,13 +14,17 @@ export const dataViewWriteString = (view: DataView, offset: number, string: stri
 
 /**
  * Writes a RIFF header buffer
- * @param {string} fourCC - 4 character code
+ * @param {string} chunkId - 4 character code
  * @param {Buffer} contentBuffer - Subchunk buffer
  * @param {number} paddingMax - Maximum pad for even RIFF byte
- *
+ * @return {Buffer} Chunk buffer
  */
-export const writeRiffChunk = (fourCC: string, contentBuffer: Buffer, paddingMax: number = 0) => {
-  if (fourCC.length !== 4) throw Error('ChunkId must be 4 characters: FourCC.')
+export const writeRiffSubChunk = (
+  chunkId: SF_SUB_CHUNKS_ID,
+  contentBuffer: Buffer,
+  paddingMax: number = 0
+) => {
+  if (chunkId.length !== 4) throw Error('ChunkId must be 4 characters: FourCC.')
   if (paddingMax % 2 !== 0) throw Error('Max padding must be even number')
 
   if (paddingMax > 0) {
@@ -27,7 +33,27 @@ export const writeRiffChunk = (fourCC: string, contentBuffer: Buffer, paddingMax
   }
 
   const instView = new DataView(new ArrayBuffer(8))
-  dataViewWriteString(instView, 0, fourCC)
-  instView.setUint32(4, 8 + contentBuffer.byteLength)
+  dataViewWriteString(instView, 0, chunkId)
+  instView.setUint32(4, contentBuffer.byteLength, true)
+  return Buffer.concat([Buffer.from(instView.buffer), contentBuffer])
+}
+
+/**
+ * Writes a RIFF header buffer
+ * @param {string} chunkId - 4 character code
+ * @param {Buffer} contentBuffer - Subchunk buffer
+ * @return {Buffer} Chunk buffer
+ */
+export const writeRiffTopChunk = (
+  chunkId: SF_TOP_CHUNKS_ID,
+  format: SF_TOP_CHUNKS_FORMAT,
+  contentBuffer: Buffer
+) => {
+  if (chunkId.length !== 4) throw Error('ChunkId must be 4 characters: FourCC.')
+
+  const instView = new DataView(new ArrayBuffer(12))
+  dataViewWriteString(instView, 0, chunkId)
+  instView.setUint32(4, 4 + contentBuffer.byteLength, true)
+  dataViewWriteString(instView, 8, format)
   return Buffer.concat([Buffer.from(instView.buffer), contentBuffer])
 }
