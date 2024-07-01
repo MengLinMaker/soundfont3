73aab4d3dd43c7528eaf879fcc101d45dc726016
MengLinMaker <menglinmaker@gmail.com>
2024-07-01 23:45:00 +1000

    Chore: Simplify jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,12 +2,7 @@ import type { Config } from 'jest'
 const config: Config = {
   testEnvironment: 'node',
   preset: 'ts-jest',
-  testMatch: ['**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js'],
-  moduleNameMapper: {
-    '~/(.*)': '<rootDir>/src/$1'
-  },
   setupFiles: ['<rootDir>/jest.setup.ts'],
-  coveragePathIgnorePatterns: ['mock.ts']
+  coveragePathIgnorePatterns: ['tests', 'index.ts']
 }
 module.exports = config
