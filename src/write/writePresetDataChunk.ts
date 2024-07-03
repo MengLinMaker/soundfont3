04e11259f1ce53887ab606df193b17db211212f7
MengLinMaker <menglinmaker@gmail.com>
2024-07-03 18:02:53 +1000

    DX: PresetData writer with test

---
+++ src/write/writePresetDataChunk.ts
@@ -0,0 +1,186 @@
+import { PresetData, SF_PDTA_CHUNKS_ID } from '..'
+import { writeRiffSubChunk, writeRiffTopChunk } from './utils'
+
+/**
+ * Writes a preset data chunk buffer.
+ * @param {string} string - raw string.
+ * @param {number} desiredByte - parsed presetData info.
+ * @return {Buffer} Elongated string buffer.
+ */
+const extendStringBuffer = (string: string, desiredByte: number) => {
+  const stringBuffer = Buffer.from(string)
+  const padLength = desiredByte - stringBuffer.byteLength
+  if (padLength < 0) return stringBuffer.slice(0, desiredByte)
+  return Buffer.concat([stringBuffer, Buffer.from(new ArrayBuffer(padLength))])
+}
+
+/**
+ * Writes a preset data chunk buffer.
+ * @param {PresetData} presetData - parsed presetData info.
+ * @return {Buffer} Chunk buffer.
+ */
+export const writePresetDataChunk = (presetData: PresetData) => {
+  let presetDataBuffer = Buffer.from('')
+
+  // Presets
+  {
+    const chunkId: SF_PDTA_CHUNKS_ID = 'phdr'
+    let loopBuffer = Buffer.from('')
+    presetData.presetHeaders.map((e) => {
+      const nameBuffer = extendStringBuffer(e.name, 20)
+      const view = new DataView(new ArrayBuffer(18))
+      view.setUint16(0, e.preset, true)
+      view.setUint16(2, e.bank, true)
+      view.setUint16(4, e.bagIndex, true)
+      view.setUint32(6, e.library, true)
+      view.setUint32(10, e.genre, true)
+      view.setUint32(14, e.morphology, true)
+      loopBuffer = Buffer.concat([loopBuffer, nameBuffer, Buffer.from(view.buffer)])
+    })
+    const chunkBuffer = writeRiffSubChunk(chunkId, loopBuffer)
+    presetDataBuffer = Buffer.concat([presetDataBuffer, chunkBuffer])
+  }
+  {
+    const chunkId: SF_PDTA_CHUNKS_ID = 'pbag'
+    let loopBuffer = Buffer.from('')
+    presetData.presetZones.map((e) => {
+      const view = new DataView(new ArrayBuffer(4))
+      view.setInt16(0, e.generatorIndex, true)
+      view.setInt16(2, e.modulatorIndex, true)
+      loopBuffer = Buffer.concat([loopBuffer, Buffer.from(view.buffer)])
+    })
+    const chunkBuffer = writeRiffSubChunk(chunkId, loopBuffer)
+    presetDataBuffer = Buffer.concat([presetDataBuffer, chunkBuffer])
+  }
+  {
+    const chunkId: SF_PDTA_CHUNKS_ID = 'pmod'
+    let loopBuffer = Buffer.from('')
+    presetData.presetModulators.map((e) => {
+      const view = new DataView(new ArrayBuffer(10))
+      view.setUint16(0, e.source.index, true)
+      view.setUint16(0, e.id, true)
+      view.setInt16(0, e.value, true)
+      view.setUint16(0, e.valueSource.index, true)
+      view.setUint16(0, e.transform, true)
+      loopBuffer = Buffer.concat([loopBuffer, Buffer.from(view.buffer)])
+    })
+    const chunkBuffer = writeRiffSubChunk(chunkId, loopBuffer)
+    presetDataBuffer = Buffer.concat([presetDataBuffer, chunkBuffer])
+  }
+  {
+    const chunkId: SF_PDTA_CHUNKS_ID = 'pgen'
+    let loopBuffer = Buffer.from('')
+    presetData.presetGenerators.map((e) => {
+      const view = new DataView(new ArrayBuffer(4))
+      view.setUint16(0, e.id, true)
+      const rangeDefined = typeof e.range !== 'undefined'
+      const valueDefined = typeof e.value !== 'undefined'
+      if (rangeDefined && valueDefined)
+        throw Error(
+          'Both "range" and "value" are defined in "pgen" when only one should be defined.'
+        )
+      else if (!rangeDefined && !valueDefined)
+        Error('Neither "range" nor "value" are defined in "pgen" when only one should be defined.')
+      else if (typeof e.range !== 'undefined') {
+        view.setUint8(2, e.range.lo)
+        view.setUint8(3, e.range.hi)
+      } else if (typeof e.value !== 'undefined') {
+        view.setUint16(2, e.value, true)
+      }
+      loopBuffer = Buffer.concat([loopBuffer, Buffer.from(view.buffer)])
+    })
+    loopBuffer = Buffer.concat([loopBuffer, Buffer.from(new ArrayBuffer(4))])
+    const chunkBuffer = writeRiffSubChunk(chunkId, loopBuffer)
+    presetDataBuffer = Buffer.concat([presetDataBuffer, chunkBuffer])
+  }
+
+  // Instruments
+  {
+    const chunkId: SF_PDTA_CHUNKS_ID = 'inst'
+    let loopBuffer = Buffer.from('')
+    presetData.instrumentHeaders.map((e) => {
+      const nameBuffer = extendStringBuffer(e.name, 20)
+      const view = new DataView(new ArrayBuffer(2))
+      view.setUint16(0, e.bagIndex, true)
+      loopBuffer = Buffer.concat([loopBuffer, nameBuffer, Buffer.from(view.buffer)])
+    })
+    const chunkBuffer = writeRiffSubChunk(chunkId, loopBuffer)
+    presetDataBuffer = Buffer.concat([presetDataBuffer, chunkBuffer])
+  }
+  {
+    const chunkId: SF_PDTA_CHUNKS_ID = 'ibag'
+    let loopBuffer = Buffer.from('')
+    presetData.instrumentZones.map((e) => {
+      const view = new DataView(new ArrayBuffer(4))
+      view.setInt16(0, e.generatorIndex, true)
+      view.setInt16(2, e.modulatorIndex, true)
+      loopBuffer = Buffer.concat([loopBuffer, Buffer.from(view.buffer)])
+    })
+    const chunkBuffer = writeRiffSubChunk(chunkId, loopBuffer)
+    presetDataBuffer = Buffer.concat([presetDataBuffer, chunkBuffer])
+  }
+  {
+    const chunkId: SF_PDTA_CHUNKS_ID = 'imod'
+    let loopBuffer = Buffer.from('')
+    presetData.instrumentModulators.map((e) => {
+      const view = new DataView(new ArrayBuffer(10))
+      view.setUint16(0, e.source.index, true)
+      view.setUint16(0, e.id, true)
+      view.setInt16(0, e.value, true)
+      view.setUint16(0, e.valueSource.index, true)
+      view.setUint16(0, e.transform, true)
+      loopBuffer = Buffer.concat([loopBuffer, Buffer.from(view.buffer)])
+    })
+    const chunkBuffer = writeRiffSubChunk(chunkId, loopBuffer)
+    presetDataBuffer = Buffer.concat([presetDataBuffer, chunkBuffer])
+  }
+  {
+    const chunkId: SF_PDTA_CHUNKS_ID = 'igen'
+    let loopBuffer = Buffer.from('')
+    presetData.instrumentGenerators.map((e) => {
+      const view = new DataView(new ArrayBuffer(4))
+      view.setUint16(0, e.id, true)
+      const rangeDefined = typeof e.range !== 'undefined'
+      const valueDefined = typeof e.value !== 'undefined'
+      if (rangeDefined && valueDefined)
+        throw Error(
+          'Both "range" and "value" are defined in "pgen" when only one should be defined.'
+        )
+      else if (!rangeDefined && !valueDefined)
+        Error('Neither "range" nor "value" are defined in "pgen" when only one should be defined.')
+      else if (typeof e.range !== 'undefined') {
+        view.setUint8(2, e.range.lo)
+        view.setUint8(3, e.range.hi)
+      } else if (typeof e.value !== 'undefined') {
+        view.setUint16(2, e.value, true)
+      }
+      loopBuffer = Buffer.concat([loopBuffer, Buffer.from(view.buffer)])
+    })
+    const chunkBuffer = writeRiffSubChunk(chunkId, loopBuffer)
+    presetDataBuffer = Buffer.concat([presetDataBuffer, chunkBuffer])
+  }
+
+  // Samples
+  {
+    const chunkId: SF_PDTA_CHUNKS_ID = 'shdr'
+    let loopBuffer = Buffer.from('')
+    presetData.sampleHeaders.map((e) => {
+      const nameBuffer = extendStringBuffer(e.name, 20)
+      const view = new DataView(new ArrayBuffer(26))
+      view.setUint32(0, e.start, true)
+      view.setUint32(4, e.end, true)
+      view.setUint32(8, e.start + e.startLoop, true)
+      view.setUint32(12, e.start + e.endLoop, true)
+      view.setUint32(16, e.sampleRate, true)
+      view.setUint8(20, e.originalPitch)
+      view.setInt8(21, e.pitchCorrection)
+      view.setUint16(22, e.link, true)
+      view.setUint16(24, e.type, true)
+      loopBuffer = Buffer.concat([loopBuffer, nameBuffer, Buffer.from(view.buffer)])
+    })
+    const chunkBuffer = writeRiffSubChunk(chunkId, loopBuffer)
+    presetDataBuffer = Buffer.concat([presetDataBuffer, chunkBuffer])
+  }
+
+  return writeRiffTopChunk('LIST', 'pdta', presetDataBuffer)
+}
--- tests/writeSoundFont2.test.ts
+++ tests/writeSoundFont2.test.ts
@@ -1,7 +1,9 @@
+import { writePresetDataChunk } from './../src/write/writePresetDataChunk'
 import { writeMetaDataChunk } from '../src/write/writeMetaDataChunk'
 import { join } from 'path'
 import { SoundFont3 } from '../src/soundFont3'
 import { readFileSync } from 'fs'
+import { SF_TOP_CHUNKS_FORMAT, SF_TOP_CHUNKS_ID } from '../src/constants'
 
 const soundFontUrl = join(__dirname, 'fonts/piano.sf2')
 const buffer = readFileSync(soundFontUrl)
@@ -11,17 +13,39 @@ describe('Write SoundFont2', () => {
   it('should write metaData', async () => {
     const metaDataBuffer = writeMetaDataChunk(soundFont.metaData)
     const metaDataChunk = soundFont.chunk.subChunks[0]
-
-    const chunkId = metaDataBuffer.slice(0, 4).toString()
-    expect(chunkId).toBe(metaDataChunk.id)
-
     const subBuffer = metaDataBuffer.slice(8, metaDataBuffer.length)
-    const expectedBuffer = Buffer.from(
-      soundFont.chunk.subChunks[0].buffer.slice(0, metaDataChunk.length)
-    )
+    const expectedBuffer = Buffer.from(metaDataChunk.buffer.slice(0, metaDataChunk.length))
+
+    const expectedChunkId: SF_TOP_CHUNKS_ID = 'LIST'
+    const chunkId = metaDataBuffer.slice(0, 4).toString()
+    expect(chunkId).toBe(expectedChunkId)
 
     const chunkLength = metaDataBuffer.readUInt32LE(4)
     expect(chunkLength).toBe(metaDataChunk.length)
+
+    const expectedFormat: SF_TOP_CHUNKS_FORMAT = 'INFO'
+    const format = subBuffer.slice(0, 4).toString()
+    expect(format).toBe(expectedFormat)
+
+    expect(subBuffer).toStrictEqual(expectedBuffer)
+  })
+
+  it('should write presetData', async () => {
+    const presetDataBuffer = writePresetDataChunk(soundFont.presetData)
+    const presetDataChunk = soundFont.chunk.subChunks[2]
+    const subBuffer = presetDataBuffer.slice(8, presetDataBuffer.length)
+    const expectedBuffer = Buffer.from(presetDataChunk.buffer.slice(0, presetDataChunk.length))
+
+    const expectedChunkId: SF_TOP_CHUNKS_ID = 'LIST'
+    const chunkId = presetDataBuffer.slice(0, 4).toString()
+    expect(chunkId).toBe(expectedChunkId)
+
+    const chunkLength = presetDataBuffer.readUInt32LE(4)
+    expect(chunkLength).toBe(presetDataChunk.length)
+
+    const expectedFormat: SF_TOP_CHUNKS_FORMAT = 'pdta'
+    const format = subBuffer.slice(0, 4).toString()
+    expect(format).toBe(expectedFormat)
 
     expect(subBuffer).toStrictEqual(expectedBuffer)
   })
