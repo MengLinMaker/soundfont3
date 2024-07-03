fe387d5363b5f0cc37523d3a3c4955b6cb1e3554
MengLinMaker <menglinmaker@gmail.com>
2024-07-03 20:39:59 +1000

    Chore: Add strictest TypeScript config

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,5 @@
 import { TextDecoder } from 'util'
+// @ts-expect-error No type declaration available
 import { AudioContext } from 'node-web-audio-api'
 // @ts-expect-error TextDecoder not included directly in Node.js
 global.TextDecoder = TextDecoder
