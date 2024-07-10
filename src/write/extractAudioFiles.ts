90bca1d75dbfd98a977889500d8a3fbdb0aa8c18
MengLinMaker <menglinmaker@gmail.com>
2024-07-11 01:05:29 +1000

    DX: Extract wav directly from data

--- src/write/extractAudioFiles.ts
+++ src/write/extractAudioFiles.ts
@@ -11,10 +11,12 @@ export const extractAudioFiles = (soundFont: SoundFont3, folderPath: string) => 
   const soundFontVersion = Number(soundFont.metaData.version)
   if (!existsSync(folderPath)) mkdirSync(folderPath)
   if (soundFontVersion >= 2 && soundFontVersion < 3) {
-    soundFont.samples.forEach((sample) => {
-      const wavFile = pcm16BufferToWav(sample.header.sampleRate, sample.data)
-      writeFileSync(`${folderPath}/${sample.header.name}.wav`, wavFile)
-    })
+    for (const header of soundFont.presetData.sampleHeaders) {
+      if (header.name === 'EOS') break
+      const data = soundFont.sampleData.slice(2 * header.start, 2 * header.end)
+      const wavFile = pcm16BufferToWav(header.sampleRate, data)
+      writeFileSync(`${folderPath}/${header.name}.wav`, wavFile)
+    }
   } else if (soundFontVersion >= 3 && soundFontVersion < 4) {
     soundFont.samples.forEach((sample) => {
       writeFileSync(`${folderPath}/${sample.header.name}.ogg`, Buffer.from(sample.data))
