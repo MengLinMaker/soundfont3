9f3825597b0cf185b3d036a81ca89325feeededa
MengLinMaker <menglinmaker@gmail.com>
2024-07-14 01:27:47 +1000

    FIx: Prefer @ts-expect-error explainations

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,6 @@
-// @ts-nocheck
 import { TextDecoder } from 'util'
 import { AudioContext } from 'node-web-audio-api'
+// @ts-expect-error TextDecoder not included directly in Node.js
 global.TextDecoder = TextDecoder
+// @ts-expect-error AudioContext not included directly in Node.js
 global.AudioContext = AudioContext
