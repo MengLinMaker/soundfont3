a97dc886e47d1e54c0137ff0b6f2c2fdb2c0c9ab
MengLinMaker <menglinmaker@gmail.com>
2024-07-14 12:10:16 +1000

    Chore: Remove unused packages

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from 'jest'
-const config: Config = {
-  testEnvironment: 'node',
-  preset: 'ts-jest',
-  setupFiles: ['<rootDir>/jest.setup.ts'],
-  coveragePathIgnorePatterns: ['tests', 'index.ts'],
-}
-module.exports = config
--- jest.setup.ts
+++
@@ -1,6 +0,0 @@
-import { TextDecoder } from 'util'
-import { AudioContext } from 'node-web-audio-api'
-// @ts-expect-error TextDecoder not included directly in Node.js
-global.TextDecoder = TextDecoder
-// @ts-expect-error AudioContext not included directly in Node.js
-global.AudioContext = AudioContext
