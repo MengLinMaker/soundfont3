000783626e62fb4b06eb488d4edb570de5f254b4
MengLinMaker <menglinmaker@gmail.com>
2024-09-08 23:56:21 +1000

    docs: demo synth with instrument selection

--- docs/.vitepress/theme/components/player.vue
+++ docs/.vitepress/theme/components/player.vue
@@ -2,14 +2,17 @@
 import { Soundfont2Sampler } from 'smplr'
 import { onMounted, ref } from 'vue'
 // @ts-expect-error File import will not have TypeScript definition.
-import soundFontUrl3 from './piano.sf3?url'
+import soundFontUrl3 from './sample.sf3?url'
 
 // Library is linked
 import { SoundFont3, toSoundFont2Web } from '.'
+import { a } from 'vitest/dist/suite-BWgaIsVn.js'
 
 const contextStarted = ref(0)
 let sampler: Soundfont2Sampler
 let soundFont: SoundFont3
+let instrumentName = ref('')
+let loadInstrument: (e: Event) => void
 
 onMounted(async () => {
   const res = await fetch(soundFontUrl3)
@@ -19,7 +22,6 @@ onMounted(async () => {
 })
 
 const startPlayer = async () => {
-  contextStarted.value = 2
   console.log('AudioContext started')
   sampler = new Soundfont2Sampler(new AudioContext(), {
     url: '',
@@ -27,7 +29,11 @@ const startPlayer = async () => {
     createSoundfont: () => soundFont,
   })
   sampler.load.then(() => {
-    sampler.loadInstrument(sampler.instrumentNames[0])
+    instrumentName.value = sampler.instrumentNames[0]
+    sampler.loadInstrument(instrumentName.value)
+    loadInstrument = () =>
+      setTimeout(() => sampler.loadInstrument(instrumentName.value))
+    contextStarted.value = 2
   })
 }
 </script>
@@ -46,36 +52,46 @@ const startPlayer = async () => {
   >
     Start synth
   </div>
-  <div v-else class="flex gap-3 text-center">
-    <div
-      @mousedown="sampler.start({ note: 84, duration: 5 })"
-      class="border grow border-red-600 hover:opacity-80 active:!opacity-50 rounded-lg p-3"
-    >
-      C6
+  <div v-else class="space-y-3">
+    <div class="space-y-1.5">
+      <div>Choose an instrument:</div>
+      <select @change="loadInstrument" v-model="instrumentName" class="ring-1 w-full ring-black ring-foreground rounded-lg p-3">
+        <option v-for="name in sampler.instrumentNames.sort((a: string, b: string) => ('' + a).localeCompare(b))">
+          {{ name }}
+        </option>
+      </select>
     </div>
-    <div
-      @mousedown="sampler.start({ note: 86, duration: 5 })"
-      class="border grow border-yellow-600 hover:opacity-80 active:!opacity-50 rounded-lg p-3"
-    >
-      D6
-    </div>
-    <div
-      @mousedown="sampler.start({ note: 88, duration: 5 })"
-      class="border grow border-green-600 hover:opacity-80 active:!opacity-50 rounded-lg p-3"
-    >
-      E6
-    </div>
-    <div
-      @mousedown="sampler.start({ note: 91, duration: 5 })"
-      class="border grow border-blue-600 hover:opacity-80 active:!opacity-50 rounded-lg p-3"
-    >
-      G6
-    </div>
-    <div
-      @mousedown="sampler.start({ note: 93, duration: 5 })"
-      class="border grow border-violet-600 hover:opacity-80 active:!opacity-50 rounded-lg p-3"
-    >
-      A7
+    <div class="flex gap-3 text-center">
+      <div
+        @mousedown="sampler.start({ note: 60, duration: 5 })"
+        class="border grow border-red-600 hover:opacity-80 active:!opacity-50 rounded-lg p-3"
+      >
+        C4
+      </div>
+      <div
+        @mousedown="sampler.start({ note: 62, duration: 5 })"
+        class="border grow border-yellow-600 hover:opacity-80 active:!opacity-50 rounded-lg p-3"
+      >
+        D4
+      </div>
+      <div
+        @mousedown="sampler.start({ note: 64, duration: 5 })"
+        class="border grow border-green-600 hover:opacity-80 active:!opacity-50 rounded-lg p-3"
+      >
+        E4
+      </div>
+      <div
+        @mousedown="sampler.start({ note: 67, duration: 5 })"
+        class="border grow border-blue-600 hover:opacity-80 active:!opacity-50 rounded-lg p-3"
+      >
+        G4
+      </div>
+      <div
+        @mousedown="sampler.start({ note: 69, duration: 5 })"
+        class="border grow border-violet-600 hover:opacity-80 active:!opacity-50 rounded-lg p-3"
+      >
+        A4
+      </div>
     </div>
   </div>
 </template>
